ddc345f91c398c668b9f90289cc7081e055d3764
ShisuiShui <obelidorjohnhenry@gmail.com>
2023-11-25 20:23:55 +0800

    first commit

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -28,6 +28,22 @@
                 <div class="">
                   <v-icon @click="toggleCommentForm(index)" :color="showCommentForm[index] ? 'blue' : 'grey' ">mdi-chat</v-icon>
                   <span class="ml-2">{{ comments[index].length }}</span>
+                  <v-icon @click="deletePost(index)" color="orange">mdi-delete</v-icon>
+                  <v-icon @click="editPost(index)" color="blue">mdi-pencil</v-icon>
+                  <v-list-item-content>
+                    <!-- Show textarea when editing post, otherwise show post content -->
+                    <template v-if="editingPost.index === index">
+                      <v-textarea v-model="editingPost.content" label="Edit post"></v-textarea>
+                      <div class="d-flex justify-end mb-3">
+                        <v-btn @click="updatePost(index)" color="primary">Update</v-btn>
+                        <v-btn @click="cancelEditPost" color="grey">Cancel</v-btn>
+                      </div>
+                    </template>
+                    <template v-else>
+                      {{posts.content }}
+                    </template>
+                  </v-list-item-content>
+
                 </div>
               </div>
               <!-- Comment input and submit button -->
@@ -47,10 +63,24 @@
                   <div class="d-flex justify-end mb-3">
                     <span class="ml-2">{{ formatDate(comment.timestamp) }}</span>
                   </div>
-                  <v-list-item-content>{{ comment.content }}</v-list-item-content>
+                  <v-list-item-content>
+                    <!-- Show textarea when editing comment, otherwise show comment content -->
+                    <template v-if="isEditingComment && editingComment.postIndex === index && editingComment.commentIndex === commentIndex">
+                      <v-textarea v-model="editingComment.content" label="Edit comment"></v-textarea>
+                    </template>
+                    <template v-else>
+                      {{ comment.content }}
+                    </template>
+                  </v-list-item-content>
                   <div class="d-flex justify-end mb-3">
-                    <v-icon @click="togglecomment(index, commentIndex)" :color="commentLiked[index][commentIndex] ? 'red' : 'gray' ">mdi-heart</v-icon>
+                    <v-icon @click="togglecomment(index, commentIndex)" :color="commentLiked[index][commentIndex] ? 'red' : 'red'">mdi-heart</v-icon>
                     <span style="margin-left: 5px;">{{ commentLikeCount[index][commentIndex] || 0 }}</span>
+                    <!-- Delete comments -->
+                    <v-icon @click="deleteComment(index, commentIndex)" color="orange">mdi-delete</v-icon>
+                    <v-icon @click="editComment(index, commentIndex, comment)" color="blue">mdi-pencil</v-icon>
+                    <div class="d-flex justify-end mb-3">
+                      <v-btn @click="updateComment" color="primary">Update</v-btn>
+                    </div>
                   </div>
                 </v-list-item>
               </v-list>
@@ -78,8 +108,19 @@ export default defineComponent({
       comments: [],
       commentLiked: [],
       commentLikeCount: [],
+      editingPost: {
+        index: -1,
+        content: "",
+      },
+      editingComment: {
+        postIndex: -1,
+        commentIndex: -1,
+        content: "",
+      },
+      isEditingComment: false,
     };
   },
+
   methods: {
     addPost() {
       if (this.postContent.trim() !== "") {
@@ -101,12 +142,12 @@ export default defineComponent({
     togglecomment(postIndex, commentIndex) {
       // If not initialized, set it to false
       if (typeof this.commentLiked[postIndex] === 'undefined') {
-        this.$set(this.commentLiked, postIndex, false);
+        this.$set(this.commentLiked, postIndex, []);
       }
 
       const commentLikedArray = this.commentLiked[postIndex];
 
-      // If not initialized, set it to 0
+      // If not initialized, set it to false
       if (typeof commentLikedArray[commentIndex] === 'undefined') {
         this.$set(commentLikedArray, commentIndex, false);
       }
@@ -123,7 +164,6 @@ export default defineComponent({
       }
     },
 
-
     toggleLike(index) {
       this.likedPosts[index] = !this.likedPosts[index];
       this.likeCount[index] += this.likedPosts[index] ? 1 : +1; // Update like count based on like status
@@ -131,6 +171,106 @@ export default defineComponent({
     toggleCommentForm(index) {
       this.showCommentForm[index] = !this.showCommentForm[index];
     },
+
+    deleteComment(postIndex, commentIndex) {
+      if (confirm("Are you sure you want to delete this post?"))
+      // Remove the comment from the array
+      this.comments[postIndex].splice(commentIndex, 1);
+    },
+
+
+    deletePost(index) {
+      // Show a confirmation dialog before deleting the post
+      if (confirm("Are you sure you want to delete this post?")) {
+        // Remove the post and related data from the arrays
+        this.posts.splice(index, 1);
+        this.likedPosts.splice(index, 1);
+        this.likeCount.splice(index, 1);
+        this.showCommentForm.splice(index, 1);
+        this.commentInput.splice(index, 1);
+        this.comments.splice(index, 1);
+        this.commentLiked.splice(index, 1);
+        this.commentLikeCount.splice(index, 1);
+
+        // You may also want to perform any additional cleanup or send a request to delete the post on the server
+      }
+    },
+    editComment(postIndex, commentIndex, comment) {
+      // Set the editingComment object to initialize the editing state
+      this.editingComment = {
+        postIndex,
+        commentIndex,
+        content: comment.content,
+      };
+      // Reset the like count for the specific comment
+      if (this.commentLikeCount[postIndex]) {
+        this.commentLikeCount[postIndex][commentIndex] = 0;
+    }
+      // Set isEditingComment to true
+      this.isEditingComment = true;
+    },
+
+    editPost(index) {
+      // Assuming you have some kind of editing mechanism, you can implement it here
+      // For example, you can open a modal or update the post content in the UI for editing
+      // You can use this.posts[index] to access the post data at the specified index
+      // You can add an "Edit" button in your UI to trigger this method
+
+      // Here's a simple example:
+      const editedContent = prompt("Edit Your Post Here:", this.posts[index].content);
+      if (editedContent !== null) {
+        // Reset the liked status to false when editing the post
+        this.likedPosts[index] = false;
+
+
+        // Reset the like count to 0
+        this.likeCount[index] = 0;
+
+        this.posts[index].content = editedContent;
+        // You can also perform additional logic such as sending an update request to the server
+      }
+    },
+
+    updateComment() {
+      // Assuming you have an "Update" button in your UI to trigger this method
+      const { postIndex, commentIndex, content } = this.editingComment;
+      // Reset the like count for the specific comment to 0
+      if (this.commentLikeCount[postIndex]) {
+        this.commentLikeCount[postIndex][commentIndex] = 0;
+      }
+      // Update the comment content
+      this.comments[postIndex][commentIndex].content = content;
+
+      // Reset the editing state
+      this.cancelEditComment();
+    },
+    cancelEditComment() {
+      // Reset the editing state
+      this.editingComment = {
+        postIndex: -1,
+        commentIndex: -1,
+        content: "",
+      };
+      // Set isEditingComment to false
+      this.isEditingComment = false;
+    },
+    updatePost(index) {
+      // Assuming you have an "Update" button in your UI to trigger this method
+      this.posts[index].content = this.editingPost.content;
+
+      // Reset the editing state
+      this.cancelEditPost();
+    },
+
+    cancelEditPost() {
+      // Reset the editing state for the main post
+      this.editingPost = {
+        index: -1,
+        content: "",
+      };
+    },
+
+
     addComment(index) {
       const commentText = this.commentInput[index];
       if (commentText.trim() !== "") {
